fce507f72aa1fafc9f8e1eafed59d3dbdc1486b3
Andrew Norman <anorman728@gmail.com>
2017-02-20 10:37:53 -0600

    Option to use mysql_secure_installation on setup.

--- setup scripts/setupMysql.sh
+++ setup scripts/setupMysql.sh
@@ -1,6 +1,14 @@
-#Install MySQL
+# Install MySQL
 
     apt-get install -y mysql-client mysql-server
+
+# Secure installation of MySQL, if y is selected.
+
+    echo "Do you want to do a secure installation of MySQL? (Y/n)"
+    read ans
+    if [ "$ans" != "n" ] ; then
+        mysql_secure_installation
+    fi
 
 # Find current directory.
 
